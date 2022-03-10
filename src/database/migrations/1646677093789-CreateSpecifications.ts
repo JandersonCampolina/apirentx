f26600d1d9df2916293e8b964a017b52bcc5fc42
Janderson Campolina <jandersoncampolina@icloud.com>
2022-03-10 16:38:19 -0300

    initial commit

---
+++ src/database/migrations/1646677093789-CreateSpecifications.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { specificationsRoutes } from "../../routes/specifications.routes";
+
+export class CreateSpecifications1646677093789 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "specifications",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("specifications");
+    }
+
+}
---
+++ src/shared/container/index.ts
@@ -0,0 +1,25 @@
+import { container } from "tsyringe";
+
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
+
+// ICategoryRepository
+container.registerSingleton<ICategoriesRepository>(
+  "CategoriesRepository",
+  CategoriesRepository
+);
+
+// ICategoryRepository
+container.registerSingleton<ISpecificationRepository>(
+  "SpecificationsRepository",
+  SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+)
